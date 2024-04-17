0ab901e5683f0154bb47562033a9e2fd032d6cd6
mildc055ee <mildc055ee@gmail.com>
2024-04-17 23:31:37 +0900

    add test

--- src/pytest_action_playground/__init__.py
+++ src/pytest_action_playground/__init__.py
@@ -1,2 +1,5 @@
 def hello() -> str:
     return "Hello from pytest-action-playground!"
+
+def add(a, b):
+    return a + b
--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,8 +1,13 @@
 import pytest
 
+import pytest_action_playground
 
 def test_demo(fashion_mnist):
     (x_train, y_train), (x_test, y_test) = fashion_mnist
 
     assert len(x_train) == len(y_train)
     assert len(x_test) == len(y_test)
+
+
+def test_add():
+    assert pytest_action_playground.add(1, 2) == 3
