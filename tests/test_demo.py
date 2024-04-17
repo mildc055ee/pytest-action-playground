204084d7a2db315257f92ae01b72e8f3a7a1dc37
mildc055ee <mildc055ee@gmail.com>
2024-04-17 15:38:27 +0900

    modify demo test

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,4 +1,8 @@
 import pytest
 
+
 def test_demo(fashion_mnist):
-    assert True
+    (x_train, y_train), (x_test, y_test) = fashion_mnist
+
+    assert len(x_train) == len(y_train)
+    assert len(x_test) == len(y_test)
